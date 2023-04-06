391b65a0865f6dd0b6a015066be2919869dbb3d5
István Nagy <istvan.nagy.88@gmail.com>
2023-04-06 19:05:46 +0200

    feat: add skeleton loader for card content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatRippleModule } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatRadioModule } from '@angular/material/radio';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MediaCardComponent } from './media-card/media-card.component';
@@ -23,7 +23,11 @@ import { SearchComponent } from './search/search.component';
     MatRadioModule,
     MatRippleModule,
     MatButtonModule,
-    MatProgressSpinnerModule,
+    NgxSkeletonLoaderModule.forRoot({
+      animation: 'pulse',
+      loadingText: 'Loading...',
+      theme: { background: '#e8e3e3', height: '16px' },
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
